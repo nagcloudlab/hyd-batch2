4ca93bbc1cefb8b7c3762609cc482859a21409e9
nagcloudlab <67089749+nagcloudlab@users.noreply.github.com>
2026-04-28 11:48:02 +0530

    refactored

--- transfer-service-v5/src/main/java/com/example/TransferServiceApplication.java
+++ transfer-service-v5/src/main/java/com/example/TransferServiceApplication.java
@@ -6,11 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import com.example.service.TransferService;
--- transfer-service-v5/src/main/java/com/example/service/TransferServiceImpl.java
+++ transfer-service-v5/src/main/java/com/example/service/TransferServiceImpl.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
